bdd0489eebb1920fec367dd6d86bbe915cc0b03a
Amarvir Singh <amarvirgill06@gmail.com>
2021-02-12 12:59:45 -0500

    first commit

---
+++ app/src/main/java/com/example/maps_amarvirsingh_c0783273/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.maps_amarvirsingh_c0783273;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
